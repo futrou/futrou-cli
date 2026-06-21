2f189dd5518b285307b26097e7fa4dc7d781dc52
Futrou Team <info@futrou.com>
2026-06-21 21:34:42 +0200

    Add npm alias package that delegates to npx futrou
    Co-Authored-By: Futrou Bot <bot@futrou.com>

---
+++ templates/npm-alias/index.cjs
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+'use strict';
+
+const { spawnSync } = require('child_process');
+
+const pkg = require('./package.json');
+const NAME = pkg.productName || pkg.name;
+
+const result = spawnSync(
+  'npx',
+  ['futrou', '--yes', ...process.argv.slice(2)],
+  { stdio: 'inherit', shell: true }
+);
+
+process.exit(result.status ?? 1);
